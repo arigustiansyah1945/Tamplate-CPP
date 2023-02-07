0e05509cd6eb6cd955867e2067ba0a066ac0e512
Ari Gustiansyah <110279655+arigustiansyah1945@users.noreply.github.com>
2023-02-07 21:42:51 +0700

    Add files via upload

---
+++ Tamplate Cpp/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+// delete main.cpp and main.exe if you want to start a project
+
+int main()
+{
+    cout << "Testing" << endl;
+    cout << "Succes" << endl;
+
+    return 0;
+}
